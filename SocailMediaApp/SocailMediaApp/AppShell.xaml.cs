187daf2fcf2820861c595ef2370fd2dc23886eb9
puthik ma <puthik.ma@gmail.com>
2020-09-19 11:14:01 -0500

    Add project files.

---
+++ SocailMediaApp/SocailMediaApp/AppShell.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SocailMediaApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SocailMediaApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            //Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
